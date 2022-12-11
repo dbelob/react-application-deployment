f7c204a51b7e722191d9be1f602ad21d58192415
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-12-11 20:52:41 +0300

    Simplify

--- react-application-deployment-server/src/main/java/application/controller/HelloController.java
+++ react-application-deployment-server/src/main/java/application/controller/HelloController.java
@@ -1,20 +1,18 @@
 package application.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 
 /**
  * Hello controller.
  */
-@Controller
+@RestController
 @RequestMapping("/api/hello")
 public class HelloController {
     @GetMapping("/message")
-    @ResponseBody
     public String getMessage(@RequestParam(required = false) String name) {
         return String.format("Hello, %s!",
                 (name != null) ? HtmlUtils.htmlEscape(name) : "world");
