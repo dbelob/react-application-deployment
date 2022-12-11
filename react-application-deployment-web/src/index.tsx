d52c49e4570890908e7a2a30d9407da555ad9398
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-12-11 23:40:21 +0300

    Fix quotes

--- react-application-deployment-web/src/index.tsx
+++ react-application-deployment-web/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
